db32754dbc4aeee5f3ad6653e6cf5f06325d7765
NavisXu <1905698198@qq.com>
2022-09-21 10:47:53 +0800

    Part 3 docx

--- Part3/src/JumperRunner.java
+++ Part3/src/JumperRunner.java
@@ -3,9 +3,9 @@ import info.gridworld.actor.*;
 public class JumperRunner {
     
     public static void main(String[] argv){
-
+        //创建world
         ActorWorld world = new ActorWorld();
-
+        //创建world中的各种属性并加入
         world.add(new Jumper());
         world.add(new Bug());
         world.add(new Rock());
