1716d667d71a1203aefb7c25602e11e640e13154
guhlong <2530266758@qq.com>
2022-10-03 17:53:46 +0800

    junit4

--- PreToolTest/MyJunitTest/junitTest/MyJunitTest0Test.java
+++ PreToolTest/MyJunitTest/junitTest/MyJunitTest0Test.java
@@ -1,7 +1,8 @@
-public class MyJunitTest0Test {
-
+import org.junit.Test;
+public class MyJunitTest0Test
+{
     MyJunitTest0 myJunitTest0 = new MyJunitTest0();
-    @org.junit.Test
+    @Test
     public void junitTest()
     {
         System.out.println("这是测试文件");
